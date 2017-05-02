a7a55826b3abddb44a6eca2070f3443372512f56
rdivahar <rsdiva@icloud.com>
2017-05-02 00:40:30 -0400

    AppDirect website new Login changes
    Adding the automation code changes for the new AppDirect site behaviour.

--- AppDirectAutomation/src/main/java/com/appdirect/pagefactory/HomePage.java
+++ AppDirectAutomation/src/main/java/com/appdirect/pagefactory/HomePage.java
@@ -1,9 +1,11 @@
 package com.appdirect.pagefactory;
 
+import java.util.ArrayList;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import com.appdirect.common.PageBase;
+import com.appdirect.constants.TitleConstants;
 import com.appdirect.utility.ExtentLogger;
 
 /**
@@ -11,14 +13,12 @@ import com.appdirect.utility.ExtentLogger;
 * @author sdivahar
 */
 public class HomePage extends PageBase {
-
-	WebDriver driver;
 	
 	public HomePage(WebDriver _driver) {
 		super(_driver);
 	}
 
-	@FindBy(xpath=".//*[@class='login']/a")
+	@FindBy(id="login")
 	private WebElement login;
 	
 	@FindBy(xpath="html/body/footer/div/div[2]/div[1]/ul[3]/li[4]/a")
@@ -33,8 +33,18 @@ public class HomePage extends PageBase {
 		}
 	}
 	
-	public void clickLogin(boolean isFooterLink){
-		
+	public void loginAndSwitchToMarketPlace(){
+		try {
+			this.clickLogin();
+			ArrayList<String> tabs_windows = new ArrayList<String> (driver.getWindowHandles());
+		    driver.switchTo().window(tabs_windows.get(1));
+		    ExtentLogger.logInfo("Switched to the new Tab OnClick Login Button = "+getPageTitle());
+		} catch (Exception e) {
+			ExtentLogger.logError("Failed While Loading the New MarketPlace Tab from the AppDirect Home");
+		}
+	}
+	
+	public void clickLogin(boolean isFooterLink){	
 		if (isFooterLink == true){
 			try {
 				loginFooter.click();
@@ -43,5 +53,23 @@ public class HomePage extends PageBase {
 				ExtentLogger.logError("Failed While Clicking Login from the Footer Area at AppDirect Home");
 			}
 		}
+	}
+	
+	public boolean verifyMarketplaceLoadedAsNewTab(){
+		Boolean bLoaded = false;		
+		ArrayList<String> tabs_windows = new ArrayList<String>(driver.getWindowHandles());
+		try {
+			if (tabs_windows.size() > 1){
+				driver.switchTo().window(tabs_windows.get(1));
+				bLoaded = driver.getTitle().equalsIgnoreCase(TitleConstants.LOGIN_PAGE);
+			}else{
+				bLoaded = false;
+				throw new Exception("Failed to Load the New Tab, the number of Open Tabs is less than Two");
+			}
+			return bLoaded;
+		} catch (Exception e) {
+			ExtentLogger.logError("Failed While Clicking Login from the Footer Area at AppDirect Home");
+			return bLoaded = false;
+		}	
 	}
 }
